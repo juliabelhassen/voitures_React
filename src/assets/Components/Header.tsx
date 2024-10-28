85a8e9fbb13126c4b2c47e332906089c06250755
BELHASSEN <julia_belhassen@hotmail.com>
2024-10-28 16:30:20 +0100

    Derniere MAJ avec footer et Header

---
+++ src/assets/Components/Header.tsx
@@ -0,0 +1,18 @@
+function Header() {
+  return (
+    <>
+      <figure>
+        <header>
+          <img id="carLogo" src="voitures_React/src/assets/Images/logovoiture.png" alt="Le logo du site"/>
+          <li>
+            <ul>Louer</ul>
+            <ul>Acheter</ul>
+            <ul>Essayer</ul>
+          </li>
+        </header>
+      </figure>
+    </>
+  );
+}
+
+export default Header;
