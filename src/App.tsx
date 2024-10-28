437338ca7b44ee02d73ff8c91d7ab8e3a8a9d08d
GaeBolga <benjamin.lefort@mail.com>
2024-10-28 16:54:51 +0100

    corrections mineurs + cheval deluxe

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import { useState } from 'react'
 import './App.css'
 import CarCard from './assets/Components/CarCard'
 import carList from './assets/Components/tableau';
-
-
+import Footer from './assets/Components/Footer';
+import Header from './assets/Components/Header';
 
 function App() {
 
   return (
     <>
+    <Header/>
 {carList.map((props, i) => <CarCard key={i} {...props} />)}
+<Footer />
+
     </>
   )
 }
--- src/assets/Components/tableau.tsx
+++ src/assets/Components/tableau.tsx
@@ -1,6 +1,6 @@
 const carList = [
   {
-  imgSrc : "",
+  imgSrc : "./src/assets/Images/chevroletcamaro-remove.png",
   modele : "Chevrolet Camaro",
   price : "107 692 €" ,
   description : "Une belle voiture"
@@ -8,7 +8,7 @@ const carList = [
   },
 
   {  
-    imgSrc : "",
+    imgSrc : "src/assets/Images/citroenc4black.png",
     modele : "Citroen C4",
     price : "28 250 € " ,
     description : "Une belle voiture"
@@ -16,9 +16,11 @@ const carList = [
   },
 
   {  
-    imgSrc : "",
+    imgSrc : "src/assets/Images/whiteTesla.png",
     modele : "Tesla",
     price : "45 600 €" ,
     description : "Une belle voiture"
 
-  },
+  }]
+
+  export default carList
