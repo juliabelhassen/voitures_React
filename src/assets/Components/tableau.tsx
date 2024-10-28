bdb98f88749158707236f53b0761ba374546e951
BELHASSEN <julia_belhassen@hotmail.com>
2024-10-28 11:33:15 +0100

    creation du tableau

---
+++ src/assets/Components/tableau.tsx
@@ -0,0 +1,25 @@
+const carList = [
+  {
+  imgSrc : "\src\assets\Images\chevroletcamaro-remove.png",
+  modele : "Chevrolet Camaro",
+  price : "107 692 €" ,
+  description : "Une belle voiture"
+
+  },
+
+  {  
+    imgSrc : "\src\assets\Images\citroenc4black.png",
+    modele : "Citroen C4",
+    price : "28 250 € " ,
+    description : "Une belle voiture"
+
+  },
+
+  {  
+    imgSrc : "\src\assets\Images\whiteTesla.png",
+    modele : "Tesla",
+    price : "45 600 €" ,
+    description : "Une belle voiture"
+
+  },
+];
