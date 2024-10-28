36ffd695a8310afb06bc003a99ea024065bea2a9
GaeBolga <benjamin.lefort@mail.com>
2024-10-28 16:26:58 +0100

    le tableau est dans le tableau, c'est bô

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import CarCard from './assets/Components/CarCard'
-
-const carList = [
-  {
-  imgSrc : "\src\assets\Images\chevroletcamaro-remove.png",
-  modele : "Chevrolet Camaro",
-  price : "107 692 €" ,
-  description : "Une belle voiture"
-
-  },
-
-  {  
-    imgSrc : "\src\assets\Images\citroenc4black.png",
-    modele : "Citroen C4",
-    price : "28 250 € " ,
-    description : "Une belle voiture"
-
-  },
-
-  {  
-    imgSrc : "C:\Users\Ezekiel\Documents\codegit\voitures_React\src\assets\Images\whiteTesla.png",
-    modele : "Tesla",
-    price : "45 600 €" ,
-    description : "Une belle voiture"
-
-  },
-];
+import carList from './assets/Components/tableau';
 
 
 
