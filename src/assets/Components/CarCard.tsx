348a209a49639e42493ac6180f57bcd5a1c3d68b
BELHASSEN <julia_belhassen@hotmail.com>
2024-10-29 12:30:08 +0100

    Final project

--- src/assets/Components/CarCard.tsx
+++ src/assets/Components/CarCard.tsx
@@ -1,38 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-function CarCard(props : {modele: string, imgSrc: string, price: string, description: string}) {
-
-  const [isExpanded, setIsExpanded] = useState(false);
+function CarCard(props: {
+  modele: string;
+  imgSrc: string;
+  price: string;
+  description: string;
+}) {
+  const [isExpanded, setIsExpanded] = useState(true);
 
   const toggleText = () => {
     setIsExpanded(!isExpanded);
   };
 
-  const Readmore = "Cette voiture, elle est trop belle ! Elle brille comme un trésor et roule super bien, comme un éclair magique ! Elle est rapide comme un guépard et ne s’arrête jamais, même si tu roules très très loin. Quand tu montes dedans, c'est comme voler dans un rêve. Les sièges sont tout doux, et le volant tourne sans faire de bruit, c’est trop parfait ! C'est la meilleure voiture du monde entier, et tout le monde va la regarder avec des grands yeux.";
-
+  const Readmore =
+    "Cette voiture, elle est trop belle ! Elle brille comme un trésor et roule super bien, comme un éclair magique ! Elle est rapide comme un guépard et ne s’arrête jamais, même si tu roules très très loin. Quand tu montes dedans, c'est comme voler dans un rêve. Les sièges sont tout doux, et le volant tourne sans faire de bruit, c’est trop parfait ! C'est la meilleure voiture du monde entier, et tout le monde va la regarder avec des grands yeux.";
 
   return (
-      <>
-<section className="cardSection">
-    <figure>
-      {props.imgSrc ? <img src={props.imgSrc} /> : <p>???</p>}
-      <figcaption>
-        <p>{props.modele}</p>
-      </figcaption>
-    
-    <p>à partir de {props.price}/mois,  
-      
-    {isExpanded ? props.description : props.description.slice(0, 40) +   Readmore  }
-     </p>
+    <>
+      <section className="cardSection">
+        <figure>
+          {props.imgSrc ? <img src={props.imgSrc} /> : <p>???</p>}
+          <p>{props.modele}</p>
+          <p>à partir de {props.price}</p>
+          <p>
+            {isExpanded
+              ? props.description
+              : props.description.slice(0, 40) + Readmore}
+          </p>
+          <button onClick={toggleText}>
+            {" "}
+            {isExpanded ? "Read more" : "Read less"}{" "}
+          </button>
+        </figure>
+      </section>
+    </>
+  );
+}
 
-    <button onClick={toggleText}>  {isExpanded ? 'Read less' : 'Read more'}  </button>  
-    </figure>
-   </section>
-
-      </>
-  )
-};
-
-export default CarCard
-
-
+export default CarCard;
